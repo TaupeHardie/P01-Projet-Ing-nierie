7e41ec4f5ddbc12ed3e1252c54c4dd30fcbc74f1
Cyril <brebisasthmatique@gmail.com>
2019-05-27 16:39:49 +0200

    Classe PDF

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,41 +1,11 @@
 package main;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
-import org.apache.pdfbox.text.PDFTextStripper;
 
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		PDDocument pdfTest = null;
-		
-		try {
-			pdfTest = PDDocument.load(new File("src/main/resources/pdf/AF MAINTENANCE/16904-ESA79087987.pdf"));
-			
-			PDFTextStripper stripper = new PDFTextStripper();
-			stripper.setSortByPosition(true);
-			
-			for (int p = 0; p < pdfTest.getNumberOfPages(); p++) {
-				stripper.setStartPage(p);
-				stripper.setEndPage(p);
-				
-				String text = stripper.getText(pdfTest);
-				System.out.println(text.trim());
-				System.out.println();
-			}
-		} catch (InvalidPasswordException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+		PDF testPDF = new PDF("src/main/resources/pdf/Betafonce/8911100750_2019-01_PY4330_SD4330[]_signed.pdf");
+		testPDF.print();
 	}
 
 }
---
+++ src/main/java/main/PDF.java
@@ -0,0 +1,105 @@
+package main;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+public class PDF {
+	/**
+	 * Objet PDDocument représentant le fichier PDF
+	 */
+	private PDDocument doc;
+	private String content = "";
+	
+	/**
+	 * Constructeur par défaut
+	 */
+	public PDF() {
+		super();
+	}
+
+	/**
+	 * Constructeur. Charge le fichier PDF à partir d'un chemin
+	 * @param fileName Chaine de caractère vers le chemin du fichier à ouvrir
+	 */
+	public PDF(String fileName) {
+		super();
+		loadPDFfromString(fileName);
+	}
+	
+	/**
+	 * Constructeur. Charge le fichier PDF à partir d'un objet
+	 * @param file Objet correspondant au PDF à charger
+	 */
+	public PDF(File file) {
+		super();
+		loadPDFfromFile(file);
+	}
+	
+	/**
+	 * Charge un PDF à partir du chemin
+	 * @param fileName Chemin du PDF
+	 */
+	public void loadPDFfromString(String fileName) {
+		try {
+			doc = PDDocument.load(new File(fileName));
+			convertToText();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Charge un PDF à partir d'un objet File
+	 * @param file Objet correspondant au PDF à charger
+	 */
+	public void loadPDFfromFile(File file) {
+		try {
+			doc = PDDocument.load(file);
+			convertToText();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Convertit le contenu du PDF en texte brut
+	 */
+	public void convertToText() {
+		PDFTextStripper stripper;
+		try {
+			stripper = new PDFTextStripper();
+			stripper.setSortByPosition(true);
+			
+			for (int p = 0; p < doc.getNumberOfPages() + 1; p++) {
+				stripper.setStartPage(p);
+				stripper.setEndPage(p);
+				
+				String text = stripper.getText(doc);
+				content += text.trim();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Affiche dans la sortie standard le texte brut du document
+	 */
+	public void print() {
+		System.out.println(content);
+	}
+	
+	/**
+	 * Retourne le contenu du document
+	 * @return Contenu du PDF en texte
+	 */
+	public String getText() {
+		return content;
+	}
+	
+
+}
