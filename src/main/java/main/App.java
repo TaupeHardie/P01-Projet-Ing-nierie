437eb6988fe0febabc2ab2d0b667d406bff9e589
POULAT Axel <axel.poulat1@gmail.com>
2019-05-29 14:21:58 +0200

    comment the main

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -4,11 +4,11 @@ import java.io.File;
 import java.util.List;
 
 import extraction.Feature;
-<<<<<<< Upstream, based on branch 'feature/Regex' of https://github.com/TaupeHardie/P01-Projet-Ing-nierie.git
+
 import resources.ResourcesLoader;
-=======
+
 import extraction.Regexp;
->>>>>>> 76abe26 removeHiddenText and its test
+
 import writer.Writer;
 
 public class App {
@@ -16,26 +16,26 @@ public class App {
 	public static final String MainPath = System.getProperty("user.home")+"\\AppData\\Local\\Qweeby\\";
 	
 	public static void main(String[] args) {
-<<<<<<< Upstream, based on branch 'feature/Regex' of https://github.com/TaupeHardie/P01-Projet-Ing-nierie.git
-		PDF testPDF = new PDF("src/main/resources/pdf/CESTIA/FR64489938837_004022_2019.pdf"); 
-		//PDF testPDF = new PDF(ResourcesLoader.loadFile("pdf/Betafonce/8911100750_2019-01_PY4330_SD4330[]_signed.pdf")); 
-		System.out.println(testPDF); 
-		List<Feature> features = testPDF.findMatches(); 
-		 
-		StringBuilder texte = new StringBuilder(); 
-		for (Feature f : features) { 
-			texte.append(f.toString()+"\r\n"); 
-		} 
-		Writer.writeTo(texte.toString(), "output.txt"); 
-=======
-		PDF testPDF = new PDF(MainPath+"pdf/CESTIA/FR64489938837_004022_2019.pdf");
-		//PDF testPDF = new PDF(ResourcesLoader.loadFile("pdf/Betafonce/8911100750_2019-01_PY4330_SD4330[]_signed.pdf"));
-		System.out.println(testPDF);
-		
-		String text=testPDF.getText();
-		String clearText= Regexp.removeHiddenText(text);
-		Writer.writeTo(testPDF.getText(), MainPath+"testcache.txt");
-		Writer.writeTo(clearText, MainPath+"testclear.txt");
->>>>>>> 76abe26 removeHiddenText and its test
+
+//		PDF testPDF = new PDF("src/main/resources/pdf/CESTIA/FR64489938837_004022_2019.pdf"); 
+//		//PDF testPDF = new PDF(ResourcesLoader.loadFile("pdf/Betafonce/8911100750_2019-01_PY4330_SD4330[]_signed.pdf")); 
+//		System.out.println(testPDF); 
+//		List<Feature> features = testPDF.findMatches(); 
+//		 
+//		StringBuilder texte = new StringBuilder(); 
+//		for (Feature f : features) { 
+//			texte.append(f.toString()+"\r\n"); 
+//		} 
+//		Writer.writeTo(texte.toString(), "output.txt"); 
+//
+//		PDF testPDF = new PDF(MainPath+"pdf/CESTIA/FR64489938837_004022_2019.pdf");
+//		//PDF testPDF = new PDF(ResourcesLoader.loadFile("pdf/Betafonce/8911100750_2019-01_PY4330_SD4330[]_signed.pdf"));
+//		System.out.println(testPDF);
+//		
+//		String text=testPDF.getText();
+//		String clearText= Regexp.removeHiddenText(text);
+//		Writer.writeTo(testPDF.getText(), MainPath+"testcache.txt");
+//		Writer.writeTo(clearText, MainPath+"testclear.txt");
+
 	}
 }
