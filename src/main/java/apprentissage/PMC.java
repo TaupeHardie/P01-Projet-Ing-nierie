ed489d7879578a6e2351adb0bf0460e797b1c3b1
Cyril <brebisasthmatique@gmail.com>
2019-06-03 10:23:44 +0200

    Création des poids avec des valeurs aléatoires

--- src/main/java/apprentissage/PMC.java
+++ src/main/java/apprentissage/PMC.java
@@ -1,5 +1,33 @@
 package apprentissage;
 
-public class PMC {
+import java.util.Random;
 
+import org.ejml.simple.SimpleMatrix;
+
+/**
+ * Classe représentant un perceptron multicouche
+ *
+ */
+public class PMC {
+	SimpleMatrix S, R, X, W, Z;
+	int nombreNeuroneEntree, nombreNeuronesCC, nombreNeuroneSortie;
+	
+	/**
+	 * Contructeur par défaut. Crée les différentes matrice et les initialise
+	 * @param nbEntree Nombre de neuronnes d'entrée
+	 * @param nbCC Nombre de neuronne dans la couche cahcée
+	 * @param nbSortie Nombre de neurone dans la couche de sortie
+	 */
+	public PMC(int nbEntree, int nbCC, int nbSortie) {
+		Random rand = new Random();
+		
+		nombreNeuronesCC = nbCC;
+		nombreNeuroneEntree = nbEntree;
+		nombreNeuroneSortie = nbSortie;
+		
+		X = new SimpleMatrix(nombreNeuroneEntree, 10);
+		
+		W = SimpleMatrix.random(nombreNeuronesCC, nombreNeuroneEntree, -1, 1, rand);
+		Z = SimpleMatrix.random(nombreNeuroneSortie, nombreNeuronesCC, -1, 1, rand);
+	}
 }
